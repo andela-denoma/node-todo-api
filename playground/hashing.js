1213c7ed7ac377a3a01a5e95bbf76c031299bbdf
Enoma Deborah <deborah.enomah@andela.co>
2017-06-22 11:43:39 +0100

    setup

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,28 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedPassword = '$2a$10$nZzpUNDD0pUg7f80y65S9OSmKHuHE69NZkE0JMqX9xzrFYUFD7ue.'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+// var data = {
+//   id: 10
+// }
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 //
 // var message = 'I am a user';
 // var hash = SHA256(message).toString();
