9b1fd220a85733cef0b5f6172f1b4d049025011a
Enoma Deborah <deborah.enomah@andela.co>
2017-06-23 11:21:16 +0100

    add login route

--- server/server.js
+++ server/server.js
@@ -110,9 +110,19 @@ app.post('/users', (req, res) => {
   })
 });
 
-
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
